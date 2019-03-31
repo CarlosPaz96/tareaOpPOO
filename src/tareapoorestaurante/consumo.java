4bbf1fe48ad56ea80ec5f7d7b3a40dde0cb2d821
Hp <Hp@Laptop>
2019-03-30 22:28:49 -0600

    tarea poo

---
+++ src/tareapoorestaurante/consumo.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tareapoorestaurante;
+import java.util.ArrayList;
+/**
+ *
+ * @author Hp
+ */
+public class consumo {
+    private ArrayList LstaComida;
+
+    public consumo(ArrayList LstaComida) {
+        this.LstaComida = LstaComida;
+    }
+
+    public void setLstaComida(ArrayList LstaComida) {
+        this.LstaComida = LstaComida;
+    }
+
+    public ArrayList getLstaComida() {
+        return LstaComida;
+    }
+    
+    
+    
+}
